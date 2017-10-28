e78014e367c2228ffe29f0b9c1e95e625474d4a6
Marco-ACarvalho <marco.acarvalho76@gmail.com>
2017-10-28 19:25:17 -0200

    first commit

---
+++ login.js
@@ -0,0 +1,6 @@
+function verificaDados(){
+    var login = document.getElementById("InputUserName").value;
+    var senha = document.getElementById("InputPassword").value;
+    
+    console.log("Login: " + login + "\nSenha: " + senha);
+}
